11a8a47b3ad8f4e319226629374a01ddb36ff378
LouisCF <112464604+LouisCF@users.noreply.github.com>
2022-08-30 14:19:10 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cnpqgyynjing.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "cf-employee"
+customregion = "us-east-1"
